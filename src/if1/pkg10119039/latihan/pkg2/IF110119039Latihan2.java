1a10c38b425c95d7f0e5e5f14bd80996a9c959f8
diky <diky@diky-PC>
2020-10-17 13:30:24 +0700

    Upload Project

---
+++ src/if1/pkg10119039/latihan/pkg2/IF110119039Latihan2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119039.latihan.pkg2;
+
+/**
+ *
+ * @author
+ * NAMA  : Diva Sabila Ramadhan
+ * KELAS : IF 1
+ * NIM   : 10119039
+ * Deskripsi Program : Deklarasi & Pengenalan Variabel
+ */
+public class IF110119039Latihan2 {
+    public static void main(String[] args) {
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; // konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil 
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel Logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+        }
+    
+}
